99d3ead1669ade66c98c5083f498e4cd29dc85ff
dujfalusi <dujfalusi@execom.eu>
2018-06-04 13:45:02 +0200

    Initial project setup

---
+++ src/test/java/eu/execom/hawaii/HawaiiApplicationTests.java
@@ -0,0 +1,16 @@
+package eu.execom.hawaii;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HawaiiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
