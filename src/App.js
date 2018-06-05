765ba5ed5b8c7175245cf70328ac3ed072d71b36
Aleksandar Beronja <aberonja@execom.eu>
2018-06-05 15:02:09 +0200

    frontend initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import palms from './palms.gif';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={palms} className="palms" alt="palms" />
+          <h1 className="App-title">Hello Hawaii</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
