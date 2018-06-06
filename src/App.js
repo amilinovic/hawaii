c88ae1ff21f2132badfc7cc101830a9e9c9559f7
Aleksandar Beronja <aberonja@execom.eu>
2018-06-06 13:46:41 +0200

    test

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <p>test</p>
         <header className="App-header">
           <img src={palms} className="palms" alt="palms" />
           <h1 className="App-title">Hello Hawaii</h1>
