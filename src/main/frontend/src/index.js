d1b27ac3a94d5544725da87270786c7777011d15
dflekac <dflekac@execom.eu>
2018-06-08 14:46:25 +0200

    removed injectGlobal from index.js, created BaseStyle file for injecting global stles

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -3,13 +3,6 @@ import ReactDOM from 'react-dom';
 import 'reset-css/reset.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { injectGlobal } from 'styled-components';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
-
-injectGlobal`
-    a {
-        text-decoration: none;
-    }
-`;
