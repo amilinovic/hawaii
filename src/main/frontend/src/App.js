405829b29596ffe84cc877ea3a092796937ca203
dflekac <dflekac@execom.eu>
2018-06-11 08:24:00 +0200

    minor change

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import Login from './pages/Login';
 import NavBar from './pages/NavBar';
 import { Switch, Route, BrowserRouter, Redirect } from 'react-router-dom';
@@ -6,15 +6,13 @@ import { Switch, Route, BrowserRouter, Redirect } from 'react-router-dom';
 export default class App extends Component {
   render() {
     return (
-      <Fragment>
-        <BrowserRouter>
-          <Switch>
-            <Redirect exact from="/" to="/leave" />
-            <Route path="/login" component={Login} />
-            <Route path="/" component={NavBar} />
-          </Switch>
-        </BrowserRouter>
-      </Fragment>
+      <BrowserRouter>
+        <Switch>
+          <Redirect exact from="/" to="/leave" />
+          <Route path="/login" component={Login} />
+          <Route path="/" component={NavBar} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
