4a3efd23e7b9838881745deef8e181dcb28310ff
Aleksandar Beronja <aberonja@execom.eu>
2018-06-12 09:41:36 +0200

    added global styles in BaseStyles.js and imported it in index.js

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -4,6 +4,9 @@ import 'reset-css/reset.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import baseStyles from './styles/BaseStyles';
+
+baseStyles();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
--- src/main/frontend/src/styles/BaseStyles.js
+++ src/main/frontend/src/styles/BaseStyles.js
@@ -1,3 +1,56 @@
-/* 
-File for the global css styles
-*/
+import { injectGlobal } from 'styled-components';
+
+export default () => injectGlobal`
+
+  @import url('https://fonts.googleapis.com/css?family=Montserrat:400,900|Roboto');
+
+*{	
+  margin: 0;	
+  padding: 0;	
+  user-select: none;	
+}	
+	
+body {	
+  -webkit-font-smoothing: antialiased;	
+}	
+	
+a {	
+  text-decoration: none;	
+  &:hover,	
+  &:focus,	
+  &:active {	
+    text-decoration: none;	
+  }	
+}	
+	
+button {	
+  border: none;	
+  &:hover,	
+  &:focus,	
+  &:active {	
+    outline: none;	
+  }	
+}	
+	
+img {	
+  max-width: 100%;	
+}	
+	
+h1 {	
+  margin: 0;	
+}	
+	
+ul {	
+  padding: 0;	
+  margin: 0;	
+}	
+	
+textarea {	
+  resize: none;	
+  overflow-y: hidden;	
+  &:focus,	
+  &:active {	
+    outline: none;	
+  }	
+}	
+`;
