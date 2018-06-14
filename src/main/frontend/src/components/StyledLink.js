e497288cd46b73300bcb85ebe122997ecf5ccebe
Aleksandar Beronja <aberonja@execom.eu>
2018-06-14 09:45:49 +0200

    added styled tools and removed base styles

--- src/main/frontend/src/components/StyledLink.js
+++ src/main/frontend/src/components/StyledLink.js
@@ -1,19 +1,20 @@
 import styled from 'styled-components';
+import { prop, ifProp } from 'styled-tools';
 
 export const StyledLink = styled.span`
-  color: ${props => (props.color ? props.color : 'white')};
+  color: ${prop('color', 'white')};
   padding: 10px;
   border-radius: 5px;
-  background: ${props => (props.background ? props.background : '#FA4C50')};
+  background: ${prop('background', '#FA4C50')};
   border: 1px solid #3c3c46;
-  display: ${props => (props.display ? props.display : 'inline-block')};
+  display: ${prop('display', 'inline-block')};
   &:before {
     content: '';
-    background-image: url(${props => (props.icon ? props.icon : '')});
-    width: ${props => (props.icon ? '20px' : '')};
-    height: ${props => (props.icon ? '20px' : '')};
+    background-image: url('${prop('icon', '')}');
+    width: ${ifProp('icon', '20px', '')};
+    height: ${ifProp('icon', '20px', '')};
     float: left;
     background-size: cover;
-    margin-right: ${props => (props.icon ? '10px' : '')};
+    margin-right: ${ifProp('icon', '10px', '')};
   }
 `;
