c3777e8b728c151bbfe7b828e7c1c84a86e0e41c
Aleksandar Beronja <aberonja@execom.eu>
2018-06-14 14:07:02 +0200

    added comment for TODO

--- src/main/frontend/src/components/InformationHeader.js
+++ src/main/frontend/src/components/InformationHeader.js
@@ -3,11 +3,12 @@ import { Container, Row, Col } from 'reactstrap';
 
 export default class InformationHeader extends Component {
   render() {
+    // TODO change mocked data with actual data
     return (
       <Container fluid={true}>
         <Row>
           <Col xs="3">
-            {/* Component with image in circle should come here once it's pr is granted */}
+            {/* Component with image in circle should come here once it's pr goes through */}
             <h3>Nikola Kavezic</h3>
             <h5>Web developer</h5>
             <h5>nkavezic@execom.eu</h5>
