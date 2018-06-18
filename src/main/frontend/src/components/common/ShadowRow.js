4f001d4431954154ea8b9511a8a7fd477624b10d
Aleksandar Beronja <aberonja@execom.eu>
2018-06-18 16:48:41 +0200

    created shadow row component, changed information header

---
+++ src/main/frontend/src/components/common/ShadowRow.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+// TODO change mocked image data with actual data
+
+export const ShadowRow = styled.div`
+  box-shadow: 1px 4px 7px #8888887d;
+  display: flex;
+  -ms-flex-wrap: wrap;
+  flex-wrap: wrap;
+  margin-right: -15px;
+  margin-left: -15px;
+  border-top: 1px solid #40404a47;
+`;
