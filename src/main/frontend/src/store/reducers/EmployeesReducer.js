0e8e7f2d52fadb25ae28c29ac822f8a434ae8f08
Aleksandar Beronja <aberonja@execom.eu>
2018-06-21 15:40:20 +0200

    removed switch case in EmployeeReducer.js

--- src/main/frontend/src/store/reducers/EmployeesReducer.js
+++ src/main/frontend/src/store/reducers/EmployeesReducer.js
@@ -1,10 +1,15 @@
 import { RECEIVE_API_DATA } from '../actions/EmployeesActions';
 
-export default (state = { fetching: '' }, { type, employeeInformation }) => {
-  switch (type) {
-    case RECEIVE_API_DATA:
-      return { fetching: 'done', employeeInformation };
-    default:
-      return state;
+const actionHandlers = {
+  [RECEIVE_API_DATA](employeeInformation) {
+    return { fetching: 'done', employeeInformation };
   }
+};
+
+export default (state = { fetching: '' }, { type, employeeInformation }) => {
+  const actionHandler = actionHandlers[type];
+  if (actionHandler) {
+    return actionHandler(employeeInformation);
+  }
+  return state;
 };
