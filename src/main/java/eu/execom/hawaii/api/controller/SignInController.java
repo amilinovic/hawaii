0fe324758252a2b08b7649ff379b23c1ae9aa7ab
Nikola Dragic <ndragic@execom.eu>
2018-06-25 10:37:37 +0200

    Token services

--- src/main/java/eu/execom/hawaii/api/controller/SignInController.java
+++ src/main/java/eu/execom/hawaii/api/controller/SignInController.java
@@ -7,10 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.common.OAuth2AccessToken;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
-import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
-import org.springframework.security.oauth2.provider.token.ConsumerTokenServices;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,32 +19,25 @@ public class SignInController {
 
   private UserService userService;
 
-  //  private AuthorizationServerTokenServices authorizationServerTokenServices;
-  //
-  //  private ConsumerTokenServices consumerTokenServices;
-
   @Autowired
-  public SignInController(UserService userService,
-      AuthorizationServerTokenServices authorizationServerTokenServices, ConsumerTokenServices consumerTokenServices) {
+  public SignInController(UserService userService) {
     this.userService = userService;
-    this.authorizationServerTokenServices = authorizationServerTokenServices;
-    this.consumerTokenServices = consumerTokenServices;
   }
 
   @GetMapping(value = "/signin")
   public ResponseEntity<Principal> signIn(Principal principal) {
-    //    OAuth2Authentication oAuth2Authentication = (OAuth2Authentication) principal;
-    //    Map<String, String> details = (Map<String, String>) oAuth2Authentication.getUserAuthentication().getDetails();
-    //    String email = details.get("email");
-    //    User user = userService.getByEmail(email);
+    //        OAuth2Authentication oAuth2Authentication = (OAuth2Authentication) principal;
+    //        Map<String, String> details = (Map<String, String>) oAuth2Authentication.getUserAuthentication().getDetails();
+    //        String email = details.get("email");
+    //        User user = userService.getByEmail(email);
     //
-    //    final Collection<GrantedAuthority> authorities = new ArrayList<>();
-    //    authorities.add(new SimpleGrantedAuthority(user.getUserRole().name()));
-    //    //    oAuth2Authentication.getUserAuthentication().getAuthorities().addAll(authorities);
-    //    //    ((OAuth2Authentication) principal).getUserAuthentication().getAuthorities().addAll(authorities);
+    //        final Collection<GrantedAuthority> authorities = new ArrayList<>();
+    //        authorities.add(new SimpleGrantedAuthority(user.getUserRole().name()));
+    //        //    oAuth2Authentication.getUserAuthentication().getAuthorities().addAll(authorities);
+    //        //    ((OAuth2Authentication) principal).getUserAuthentication().getAuthorities().addAll(authorities);
     //
-    //    final Authentication authentication = new PreAuthenticatedAuthenticationToken(principal, null, authorities);
-    //    SecurityContextHolder.getContext().setAuthentication(authentication);
+    //        final Authentication authentication = new PreAuthenticatedAuthenticationToken(principal, null, authorities);
+    //        SecurityContextHolder.getContext().setAuthentication(authentication);
 
     HttpHeaders headers = new HttpHeaders();
     headers.setLocation(URI.create("http://localhost:3000"));
@@ -69,18 +58,6 @@ public class SignInController {
       return HttpStatus.UNAUTHORIZED;
     }
     return HttpStatus.OK;
-  }
-
-  @GetMapping("/logout")
-  public ResponseEntity logout(Principal principal) {
-
-    OAuth2AccessToken token = authorizationServerTokenServices.getAccessToken((OAuth2Authentication) principal);
-    consumerTokenServices.revokeToken(token.getValue());
-
-    HttpHeaders headers = new HttpHeaders();
-    headers.setLocation(URI.create("http://localhost:3000"));
-
-    return new ResponseEntity<>(headers, HttpStatus.FOUND);
   }
 
 }
