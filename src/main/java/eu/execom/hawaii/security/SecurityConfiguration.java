59d5b323818acc5cfe08eddc78711cf6a649d58e
dujfalusi <dujfalusi@execom.eu>
2018-07-10 13:42:11 +0200

    Added swagger

--- src/main/java/eu/execom/hawaii/security/SecurityConfiguration.java
+++ src/main/java/eu/execom/hawaii/security/SecurityConfiguration.java
@@ -6,26 +6,26 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @Configuration
 @EnableOAuth2Sso
 @EnableWebSecurity
+@EnableSwagger2
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.csrf()
-        .disable()
+    http.csrf().disable()
         .authorizeRequests()
-        .antMatchers("/", "/welcome", "/authentication", "/signin")
-        .permitAll()
-        .anyRequest()
-        .authenticated()
+          .antMatchers("/authentication", "/swagger-ui.html", "/swagger-resources/**", "/v2/**", "/webjars/**").permitAll()
+          .anyRequest().authenticated()
         .and()
-        .logout()
-        .invalidateHttpSession(true)
-        .deleteCookies("JSESSIONID")
-        .logoutSuccessUrl("http://localhost:3000/")
-        .permitAll();
+          .logout()
+          .invalidateHttpSession(true)
+          .deleteCookies("JSESSIONID")
+          .logoutSuccessUrl("http://localhost:3000/")
+          .permitAll();
   }
 
 }
