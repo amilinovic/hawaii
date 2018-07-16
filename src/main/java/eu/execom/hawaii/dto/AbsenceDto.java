df04f5d233ad2298cfa044e25b829adeca091680
Nikola Dragic <ndragic@execom.eu>
2018-07-16 14:04:05 +0200

    Added AbsenceType field with value from discriminator column to Absence abstract class

--- src/main/java/eu/execom/hawaii/dto/AbsenceDto.java
+++ src/main/java/eu/execom/hawaii/dto/AbsenceDto.java
@@ -2,14 +2,10 @@ package eu.execom.hawaii.dto;
 
 import eu.execom.hawaii.model.absence.Absence;
 import eu.execom.hawaii.model.absence.AbsenceType;
-import eu.execom.hawaii.model.absence.BonusDays;
 import eu.execom.hawaii.model.absence.Leave;
-import eu.execom.hawaii.model.absence.Sickness;
 import lombok.Data;
 
-import static eu.execom.hawaii.model.absence.AbsenceType.BONUS_DAYS;
 import static eu.execom.hawaii.model.absence.AbsenceType.LEAVE;
-import static eu.execom.hawaii.model.absence.AbsenceType.SICKNESS;
 
 @Data
 public class AbsenceDto {
@@ -26,14 +22,11 @@ public class AbsenceDto {
     this.name = absence.getName();
     this.comment = absence.getComment();
     this.active = absence.isActive();
+    this.absenceType = absence.getAbsenceType();
 
-    if (absence instanceof Leave) {
-      this.absenceType = LEAVE;
+    if (this.absenceType == LEAVE) {
       this.deducted = ((Leave) absence).isDeducted();
-    } else if (absence instanceof Sickness) {
-      this.absenceType = SICKNESS;
-    } else if (absence instanceof BonusDays) {
-      this.absenceType = BONUS_DAYS;
     }
   }
+
 }
