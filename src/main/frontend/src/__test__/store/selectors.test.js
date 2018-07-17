62ee4596e0144390552fe7c0a5855f8cd8621035
dflekac <dflekac@execom.eu>
2018-07-17 13:46:48 +0200

    added test for fetch employee selector

--- src/main/frontend/src/__test__/store/selectors.test.js
+++ src/main/frontend/src/__test__/store/selectors.test.js
@@ -1,13 +1,23 @@
-import { getEmployee } from '../../store/Selectors';
+import { getEmployee, getFetching } from '../../store/Selectors';
 
-describe('Get Employee Selector', () => {
-  it('should return state of employee', () => {
-    const state = {
-      employee: {
-        name: 'John',
-        surname: 'Doe'
-      }
-    };
-    expect(getEmployee(state)).toEqual({ name: 'John', surname: 'Doe' });
+describe('Employee Selector', () => {
+  describe('Get Employee Selector', () => {
+    it('should return state of employee', () => {
+      const state = {
+        employee: {
+          name: 'John',
+          surname: 'Doe'
+        }
+      };
+      expect(getEmployee(state)).toEqual({ name: 'John', surname: 'Doe' });
+    });
+  });
+  describe('Fetch Employee Selector', () => {
+    it('should return fetch state of employee', () => {
+      const state = {
+        employee: ''
+      };
+      expect(getFetching(state)).toEqual();
+    });
   });
 });
