66ccf640a0216b9c2cb798d54a2b642b780f99aa
dflekac <dflekac@execom.eu>
2018-07-17 11:09:03 +0200

    added test for selector

---
+++ src/main/frontend/src/__test__/store/selectors.test.js
@@ -0,0 +1,13 @@
+import { getEmployee, getFetching } from '../../store/Selectors';
+
+describe('Get Employee Selector', () => {
+  it('should return state of employee', () => {
+    const state = {
+      employee: {
+        name: 'John',
+        surname: 'Doe'
+      }
+    };
+    expect(getEmployee(state)).toEqual({ name: 'John', surname: 'Doe' });
+  });
+});
