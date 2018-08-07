e661d1d770899d09a82fb5eb35ac81a38acbb856
dujfalusi <dujfalusi@execom.eu>
2018-08-07 16:00:09 +0200

    Fixed requestDto

--- src/main/java/eu/execom/hawaii/dto/RequestDto.java
+++ src/main/java/eu/execom/hawaii/dto/RequestDto.java
@@ -12,16 +12,18 @@ import lombok.Data;
 @Data
 public class RequestDto {
 
+  private Long id;
   private Long userId;
-  private Long absenceId;
+  private AbsenceDto absence;
   private RequestStatus requestStatus;
   private String reason;
   @NotNull
   private List<DayDto> days;
 
   public RequestDto(Request request) {
+    this.id = request.getId();
     this.userId = request.getUser().getId();
-    this.absenceId = request.getAbsence().getId();
+    this.absence = new AbsenceDto(request.getAbsence());
     this.requestStatus = request.getRequestStatus();
     this.reason = request.getReason();
     this.days = request.getDays().stream().map(DayDto::new).collect(Collectors.toList());
