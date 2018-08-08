572702f4f20d953bd0d6131af1e11a29eeaa8100
dujfalusi <dujfalusi@execom.eu>
2018-08-08 12:43:06 +0200

    Added initial tests

--- src/test/java/eu/execom/hawaii/service/AllowanceServiceTest.java
+++ src/test/java/eu/execom/hawaii/service/AllowanceServiceTest.java
@@ -1,14 +1,98 @@
 package eu.execom.hawaii.service;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import eu.execom.hawaii.repository.AllowanceRepository;
+
+@RunWith(MockitoJUnitRunner.class)
 public class AllowanceServiceTest {
 
+  @Mock
+  AllowanceRepository allowanceRepository;
+
+  @InjectMocks
+  AllowanceService allowanceService;
+
+
   @Test
-  public void getByUser() {
+  public void shouldAllowanceByUser() {
+    // given
+
+    // when
+
+
+    // then
+
   }
 
   @Test
-  public void applyRequest() {
+  public void shouldApplyRequestWithAbsenceTypeDeducted() {
+
+    // given
+
+    // when
+
+    // then
+
   }
+
+  @Test
+  public void shouldApplyRequestWithAbsenceSubtypeAnnual() {
+
+    // given
+
+    // when
+
+    // then
+
+  }
+
+  @Test
+  public void shouldApplyRequestWithAbsenceSubtypeTraining() {
+
+    // given
+
+    // when
+
+    // then
+
+  }
+
+  @Test
+  public void shouldApplyRequestWithAbsenceTypeSickness() {
+
+    // given
+
+    // when
+
+    // then
+
+  }
+
+  @Test
+  public void shouldApplyRequestWithAbsenceTypeBonusDays() {
+
+    // given
+
+    // when
+
+    // then
+
+  }
+
+  @Test
+  public void shouldFailApplyRequestDueWrongAbsenceSubtype() {
+
+    // given
+
+    // when
+
+    // then
+
+  }
+
 }
