4abae0bb343a3c08226063f8197c84b8890ea6a6
OliveraBoskov <boskov.olivera@gmail.com>
2018-11-01 13:52:30 +0100

    removed all security

--- src/main/java/eu/execom/hawaii/api/controller/SignInController.java
+++ src/main/java/eu/execom/hawaii/api/controller/SignInController.java
@@ -1,99 +1,25 @@
 package eu.execom.hawaii.api.controller;
 
-import eu.execom.hawaii.dto.UserDto;
 import eu.execom.hawaii.model.GenericResponse;
 import eu.execom.hawaii.model.User;
-import eu.execom.hawaii.security.TokenStore;
 import eu.execom.hawaii.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
-import org.springframework.social.google.api.Google;
-import org.springframework.social.google.api.impl.GoogleTemplate;
-import org.springframework.social.google.api.plus.Person;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.Collection;
-import java.util.Collections;
-
-import static eu.execom.hawaii.security.AuthenticationFilter.AUTHENTICATION_TOKEN_KEY;
-
 @RestController
 public class SignInController {
 
-  private static final String EXECOM_DOMAIN = "execom.eu";
-
   private UserService userService;
 
-  private TokenStore tokenStore;
-
   @Autowired
-  public SignInController(UserService userService, TokenStore tokenStore) {
+  public SignInController(UserService userService) {
     this.userService = userService;
-    this.tokenStore = tokenStore;
-  }
-
-  @GetMapping(value = "/signin")
-  public ResponseEntity<UserDto> signIn(@RequestHeader(value = "Authorization") String token) {
-
-    Google google = new GoogleTemplate(token);
-    Person profile = google.plusOperations().getGoogleProfile();
-    String email = profile.getAccountEmail();
-
-    if (!email.endsWith(EXECOM_DOMAIN)) {
-      return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-    }
-
-    User user = userService.findByEmail(email);
-
-    if (!user.isActive()) {
-      return new ResponseEntity<>(HttpStatus.FORBIDDEN);
-    }
-
-    Collection<GrantedAuthority> authorities = Collections.singletonList(
-        new SimpleGrantedAuthority(user.getUserRole().name()));
-
-    Authentication authentication = new PreAuthenticatedAuthenticationToken(user, null, authorities);
-    SecurityContextHolder.getContext().setAuthentication(authentication);
-
-    String sessionToken = tokenStore.createToken(user);
-
-    HttpHeaders headers = new HttpHeaders();
-    headers.add(AUTHENTICATION_TOKEN_KEY, sessionToken);
-    headers.add("role", user.getUserRole().name());
-
-    UserDto userDto = new UserDto(user);
-
-    return new ResponseEntity<>(userDto, headers, HttpStatus.OK);
-  }
-
-  @GetMapping("/authentication")
-  public ResponseEntity authenticate(HttpServletRequest request) {
-    HttpStatus status = authenticationStatus(request);
-    return new ResponseEntity(status);
-  }
-
-  private HttpStatus authenticationStatus(HttpServletRequest request) {
-    Object authentication = SecurityContextHolder.getContext().getAuthentication();
-
-    if (authentication instanceof AnonymousAuthenticationToken) {
-      return HttpStatus.UNAUTHORIZED;
-    }
-    return HttpStatus.OK;
   }
 
   @PutMapping("/token")
--- src/main/java/eu/execom/hawaii/security/SecurityConfiguration.java
+++ src/main/java/eu/execom/hawaii/security/SecurityConfiguration.java
@@ -1,26 +1,11 @@
 package eu.execom.hawaii.security;
 
-import eu.execom.hawaii.model.User;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-import static eu.execom.hawaii.security.AuthenticationFilter.AUTHENTICATION_TOKEN_KEY;
 
 @Configuration
 @EnableOAuth2Sso
@@ -33,55 +18,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     //@formatter:off
     http.csrf().disable()
         .authorizeRequests()
-          .antMatchers("/", "/icons/**", "/welcome", "/signin", "/authentication", "/swagger-ui.html", "/swagger-resources/**",
-            "/v2/**", "/webjars/**").permitAll()
-          .antMatchers("/users", "/users/**", "/teams", "/teams/**", "/leavetypes", "/leavetypes/**", "/leaveprofiles",
-            "/leaveprofiles/**, /requests/**").hasAuthority("HR_MANAGER")
-          .anyRequest().authenticated()
-        .and()
-          .logout()
-          .invalidateHttpSession(true)
-          .deleteCookies("JSESSIONID")
-          .logoutSuccessHandler(new CustomLogoutSuccessHandler())
-          .logoutSuccessUrl("http://localhost:3000/")
-          .permitAll()
-        .and()
-        .addFilterBefore(
-          new AuthenticationFilter(authenticationManager(), tokenStore()),
-          BasicAuthenticationFilter.class);
+          .anyRequest().permitAll();
     //@formatter:on
   }
-
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth) {
-    auth.authenticationProvider(new CustomAuthenticationProvider());
-  }
-
-  @Bean
-  public TokenStore tokenStore() {
-    return new TokenStore();
-  }
-
-  private class CustomAuthenticationProvider implements AuthenticationProvider {
-    @Override
-    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-      return authentication;
-    }
-
-    @Override
-    public boolean supports(Class<?> authentication) {
-      return true;
-    }
-  }
-
-  private class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
-    @Override
-    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
-        throws IOException, ServletException {
-      String token = request.getHeader(AUTHENTICATION_TOKEN_KEY);
-      User user = tokenStore().getUser(token);
-      tokenStore().removeTokensForUser(user);
-    }
-  }
-
 }
