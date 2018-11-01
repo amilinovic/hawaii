a22035282fbecd3fb91199954d9c63da9689caf6
OliveraBoskov <boskov.olivera@gmail.com>
2018-11-01 14:38:18 +0100

    Adds test which verify that 401 response is returned when ID token request header is not set.

---
+++ src/test/java/eu/execom/hawaii/security/ApiSecurityTests.java
@@ -0,0 +1,38 @@
+package eu.execom.hawaii.security;
+
+import eu.execom.hawaii.service.UserService;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ApiSecurityTests {
+
+  @Autowired
+  private TestRestTemplate restTemplate;
+
+  @LocalServerPort
+  private int port;
+
+  @MockBean
+  private UserService userService;
+
+  @Test
+  public void shouldReturnUnauthorizedStatusCodeWhenIdTokenHeaderIsNotSet() {
+    String url = "http://localhost:" + port + "/users";
+
+    ResponseEntity<String> response = this.restTemplate.getForEntity(url, String.class);
+
+    Assert.assertThat(response.getStatusCode(), Matchers.is(HttpStatus.UNAUTHORIZED));
+  }
+}
