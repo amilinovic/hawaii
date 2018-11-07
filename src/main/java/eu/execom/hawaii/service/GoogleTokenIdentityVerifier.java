fd40e43135aecc5a258f4ddd64834d131f3d0f3c
OliveraBoskov <boskov.olivera@gmail.com>
2018-11-07 13:56:23 +0100

    We now return empty token identity when Google verifier throws exception.

--- src/main/java/eu/execom/hawaii/service/GoogleTokenIdentityVerifier.java
+++ src/main/java/eu/execom/hawaii/service/GoogleTokenIdentityVerifier.java
@@ -1,11 +1,26 @@
 package eu.execom.hawaii.service;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
 @Component public class GoogleTokenIdentityVerifier implements TokenIdentityVerifier {
+    private final GoogleIdTokenVerifier googleVerifier;
+
+    public GoogleTokenIdentityVerifier(GoogleIdTokenVerifier googleVerifier) {
+        this.googleVerifier = googleVerifier;
+    }
+
     @Override public Optional<String> tryToGetIdentityOf(String token) {
-        throw new UnsupportedOperationException();
+        GoogleIdToken idToken = null;
+        try {
+            idToken = googleVerifier.verify(token);
+        } catch (Exception e) {
+
+        }
+
+        return Optional.empty();
     }
 }
---
+++ src/test/java/eu/execom/hawaii/service/GoogleTokenIdentityVerifierTest.java
@@ -0,0 +1,37 @@
+package eu.execom.hawaii.service;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.BDDMockito.given;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GoogleTokenIdentityVerifierTest {
+    private static final String SAMPLE_ID_TOKEN = "-- id token --";
+
+    @Mock
+    private GoogleIdTokenVerifier googleVerifier;
+
+    @InjectMocks
+    private GoogleTokenIdentityVerifier googleTokenIdentityVerifier;
+
+    @Test
+    public void shouldReturnEmptyTokenIdentityWhenGoogleVerifierThrowsException()
+            throws GeneralSecurityException, IOException {
+        given(googleVerifier.verify(SAMPLE_ID_TOKEN)).willThrow();
+
+        Optional<String> tokenIdentity = googleTokenIdentityVerifier.tryToGetIdentityOf(SAMPLE_ID_TOKEN);
+
+        assertThat(tokenIdentity.isPresent(), is(false));
+    }
+}
