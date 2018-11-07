3c81c327c81433c82dff832e9299181316050019
OliveraBoskov <boskov.olivera@gmail.com>
2018-11-07 14:17:06 +0100

    We now return empty token identity when GoogleTokenId is null.

--- src/main/java/eu/execom/hawaii/service/GoogleTokenIdentityVerifier.java
+++ src/main/java/eu/execom/hawaii/service/GoogleTokenIdentityVerifier.java
@@ -20,7 +20,6 @@ import java.util.Optional;
         } catch (Exception e) {
 
         }
-
         return Optional.empty();
     }
 }
--- src/test/java/eu/execom/hawaii/service/GoogleTokenIdentityVerifierTest.java
+++ src/test/java/eu/execom/hawaii/service/GoogleTokenIdentityVerifierTest.java
@@ -15,21 +15,23 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.BDDMockito.given;
 
-@RunWith(MockitoJUnitRunner.class)
-public class GoogleTokenIdentityVerifierTest {
+@RunWith(MockitoJUnitRunner.class) public class GoogleTokenIdentityVerifierTest {
     private static final String SAMPLE_ID_TOKEN = "-- id token --";
 
-    @Mock
-    private GoogleIdTokenVerifier googleVerifier;
+    @Mock private GoogleIdTokenVerifier googleVerifier;
 
-    @InjectMocks
-    private GoogleTokenIdentityVerifier googleTokenIdentityVerifier;
+    @InjectMocks private GoogleTokenIdentityVerifier googleTokenIdentityVerifier;
 
-    @Test
-    public void shouldReturnEmptyTokenIdentityWhenGoogleVerifierThrowsException()
+    @Test public void shouldReturnEmptyTokenIdentityWhenGoogleVerifierThrowsException()
             throws GeneralSecurityException, IOException {
         given(googleVerifier.verify(SAMPLE_ID_TOKEN)).willThrow();
 
+        Optional<String> tokenIdentity = googleTokenIdentityVerifier.tryToGetIdentityOf(SAMPLE_ID_TOKEN);
+
+        assertThat(tokenIdentity.isPresent(), is(false));
+    }
+
+    @Test public void shouldReturnEmptyTokenIdentityWhenGoogleIdTokenIsNull() {
         Optional<String> tokenIdentity = googleTokenIdentityVerifier.tryToGetIdentityOf(SAMPLE_ID_TOKEN);
 
         assertThat(tokenIdentity.isPresent(), is(false));
