c037d4110f47cbe862fa01cb89658205d63ac692
OliveraBoskov <boskov.olivera@gmail.com>
2018-11-22 08:22:50 +0100

    Added id of the public holiday in PublicHolidayDto, so that on client side it can be removed by id.

--- src/main/java/eu/execom/hawaii/dto/PublicHolidayDto.java
+++ src/main/java/eu/execom/hawaii/dto/PublicHolidayDto.java
@@ -1,18 +1,20 @@
 package eu.execom.hawaii.dto;
 
-import java.time.LocalDate;
-
 import eu.execom.hawaii.model.PublicHoliday;
 import lombok.Data;
+
+import java.time.LocalDate;
 
 @Data
 public class PublicHolidayDto {
 
+  private Long id;
   private boolean active;
   private String name;
   private LocalDate date;
 
   public PublicHolidayDto(PublicHoliday publicHoliday) {
+    this.id = publicHoliday.getId();
     this.active = publicHoliday.isActive();
     this.name = publicHoliday.getName();
     this.date = publicHoliday.getDate();
