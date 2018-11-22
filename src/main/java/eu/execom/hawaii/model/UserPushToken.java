56c39d9d56c8e18a9fa485e62af9019df14cdf99
OliveraBoskov <boskov.olivera@gmail.com>
2018-11-22 09:43:52 +0100

    Updated UserPushToken entity with new column createDateTime. Eeach time new object (UserPushToken) is created, it sets the creation date and time.

--- src/main/java/eu/execom/hawaii/model/UserPushToken.java
+++ src/main/java/eu/execom/hawaii/model/UserPushToken.java
@@ -3,6 +3,7 @@ package eu.execom.hawaii.model;
 import eu.execom.hawaii.model.enumerations.Platform;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -12,6 +13,7 @@ import javax.persistence.InheritanceType;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 @Entity
 @Data
@@ -32,4 +34,7 @@ public class UserPushToken extends BaseEntity implements Serializable {
   @NotNull
   @Enumerated(EnumType.STRING)
   private Platform platform;
+
+  @CreationTimestamp
+  private LocalDateTime createDateTime;
 }
