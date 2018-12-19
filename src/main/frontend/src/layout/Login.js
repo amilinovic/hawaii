9b3d82e85eb954a7187f1437312a48c67b78bda9
Vladimir Rackovic <vrackovic@execom.eu>
2018-12-19 14:13:07 +0100

    Added simple styling to login page

--- src/main/frontend/src/layout/Login.js
+++ src/main/frontend/src/layout/Login.js
@@ -7,11 +7,27 @@ import { requestToken } from '../store/actions/getTokenActions';
 import { getAuthorization } from '../store/selectors';
 import store from '../store/store';
 import { push } from 'connected-react-router';
+import HawaiiWallpaper from '../img/hawaii_wallpaper.jpg';
+import styled, { withTheme } from 'styled-components';
+import { redirect } from '../store/sagas/getTokenFromSessionStorageSaga';
 
 const loginButtonStyle = {
   padding: '0',
-  cursor: 'pointer'
+  cursor: 'pointer',
+  backgroundColor: '#E45052'
 };
+
+const LoginContainer = styled.div`
+  background: url(${HawaiiWallpaper});
+  height: 100%;
+  width: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
 
 class Login extends Component {
   componentDidUpdate() {
@@ -22,20 +38,35 @@ class Login extends Component {
 
   render() {
     return (
-      <div className="align-items-center justify-content-center d-flex flex-grow-1 flex-column">
-        <h1>Hello Hawaii</h1>
-        <GoogleLogin
-          prompt="select_account"
-          style={loginButtonStyle}
-          disabledStyle
-          clientId="91011414864-oscjl6qmm6qds4kuvvh1j991rgvker3h.apps.googleusercontent.com"
-          onSuccess={this.props.requestToken}
-          // TODO error handling
-          // onFailure={this.props.receiveGoogleData}
+      <LoginContainer>
+        <div
+          style={{
+            position: 'absolute',
+            width: '500px',
+            height: '200px',
+            backgroundColor: 'rgba(0,0,0,0.7)',
+            borderRadius: '10px',
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-evenly',
+            alignItems: 'center',
+            color: 'white'
+          }}
         >
-          <NavigationLink>Log in</NavigationLink>
-        </GoogleLogin>
-      </div>
+          <h1>Aloha</h1>
+          <GoogleLogin
+            prompt="select_account"
+            style={loginButtonStyle}
+            disabledStyle
+            clientId="91011414864-oscjl6qmm6qds4kuvvh1j991rgvker3h.apps.googleusercontent.com"
+            onSuccess={this.props.requestToken}
+            // TODO error handling
+            // onFailure={this.props.receiveGoogleData}
+          >
+            <NavigationLink>Log in</NavigationLink>
+          </GoogleLogin>
+        </div>
+      </LoginContainer>
     );
   }
 }
