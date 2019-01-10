dd9722b990ba96261c47442320b667fd654c4499
Stefan Tolic Jonathan <stolic@execom.eu>
2019-01-10 15:57:34 +0100

    Implemented suggestions mentioned in the comments of #143
    	modified:   src/main/java/eu/execom/hawaii/model/User.java

--- src/main/java/eu/execom/hawaii/model/User.java
+++ src/main/java/eu/execom/hawaii/model/User.java
@@ -6,13 +6,22 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Lob;
+import javax.persistence.Basic;
+import javax.persistence.FetchType;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.List;
-import java.sql.Blob;
 
 @Entity
 @Data
@@ -57,7 +66,7 @@ public class User extends BaseEntity implements Serializable {
 
   @Lob
   @Basic(fetch = FetchType.LAZY)
-  @Column(name = "IMAGE", columnDefinition="MEDIUMBLOB")
+  @Column(columnDefinition="MEDIUMBLOB")
   private byte[] image;
 
   @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
