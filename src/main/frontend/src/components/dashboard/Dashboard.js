a5e9cb27c5b1369cb49488eb52dfbcbcaa1f69a8
Aleksandar Beronja <aberonja@execom.eu>
2019-02-06 12:57:47 +0100

    created administration page and moved teams and employees there, redirected to the dashboard on login where calendar will be

--- src/main/frontend/src/components/dashboard/Dashboard.js
+++ src/main/frontend/src/components/dashboard/Dashboard.js
@@ -1,48 +1,10 @@
 import React, { Component } from 'react';
-import Tabs from '../tabs/Tabs';
-import Employees from '../employees/Employees';
-import Teams from '../teams/Teams';
-import styled from 'styled-components';
-
-const DashboardContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-  width: 100%;
-  padding: 20px;
-`;
-
-const TabContent = styled.div`
-  flex: 1;
-  width: 100%;
-  padding-top: 16px;
-`;
-
 export default class Dashboard extends Component {
-  state = {
-    tabList: [
-      { label: 'Teams', content: <Teams /> },
-      { label: 'Employees', content: <Employees /> }
-    ],
-    activeTabIndex: 0
-  };
-
-  handleTabClick = index => {
-    this.setState({ activeTabIndex: index });
-  };
-
   render() {
-    const { activeTabIndex } = this.state;
-    const activeItem = this.state.tabList[activeTabIndex];
     return (
-      <DashboardContainer>
-        <Tabs
-          handleTabClick={this.handleTabClick}
-          data={this.state.tabList}
-          activeTabIndex={activeTabIndex}
-        />
-        <TabContent>{activeItem.content}</TabContent>
-      </DashboardContainer>
+      <div className="d-flex h-100 align-items-center justify-content-center">
+        Dashboard - Execom calendar
+      </div>
     );
   }
 }
