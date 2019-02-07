f85be33315e1d1c394e814644be3fb461487d656
Aleksandar Beronja <aberonja@execom.eu>
2019-02-07 13:39:15 +0100

    added prefix for requests, fixed package json build

--- src/main/frontend/src/store/services/employeesService.js
+++ src/main/frontend/src/store/services/employeesService.js
@@ -1,9 +1,10 @@
 import request from 'superagent';
+import { getLink } from '../getLink';
 
 export const getEmployeesApi = async () => {
   try {
     return await request
-      .get('/users')
+      .get(getLink('/users'))
       .set('X-ID-TOKEN', sessionStorage.getItem('token'))
       .then(res => res.body);
   } catch (e) {
