318a178b416f328ee2f3d1ec7f3fd7b98f48850d
Aleksandar Beronja <aberonja@execom.eu>
2019-02-08 14:01:18 +0100

    removed extra div in the tabs component

--- src/main/frontend/src/components/tabs/Tabs.js
+++ src/main/frontend/src/components/tabs/Tabs.js
@@ -2,18 +2,16 @@ import React from 'react';
 import Tab from '../tabs/Tab';
 
 const Tabs = ({ activeTabIndex, data, handleTabClick }) => (
-  <div>
-    <div className="d-flex flex-row flex-grow-1">
-      {data.map(({ label }, index) => (
-        <Tab
-          key={index}
-          label={label}
-          isActive={activeTabIndex === index}
-          handleTabClick={handleTabClick}
-          tabIndex={index}
-        />
-      ))}
-    </div>
+  <div className="d-flex flex-row flex-grow-1">
+    {data.map(({ label }, index) => (
+      <Tab
+        key={index}
+        label={label}
+        isActive={activeTabIndex === index}
+        handleTabClick={handleTabClick}
+        tabIndex={index}
+      />
+    ))}
   </div>
 );
 
