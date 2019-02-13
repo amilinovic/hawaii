6c1bd83a20ecc2b362aa4200be2439315bde1048
Aleksandar Beronja <aberonja@execom.eu>
2019-02-13 17:17:30 +0100

    added react-final-forms and final-forms-arrays, and fixed form for team editing

--- src/main/frontend/src/pages/EditTeam.js
+++ src/main/frontend/src/pages/EditTeam.js
@@ -1,4 +1,6 @@
+import arrayMutators from 'final-form-arrays';
 import React, { Component } from 'react';
+import { Field, Form } from 'react-final-form';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import withResetOnNavigate from '../components/HOC/withResetOnNavigate';
@@ -7,113 +9,76 @@ import { requestTeam, updateTeam } from '../store/actions/teamActions';
 import { getEmployees, getTeam } from '../store/selectors';
 
 class EditTeam extends Component {
-  state = {
-    team: {
-      name: '',
-      teamApprovers: [],
-      users: []
-    }
-  };
-
   componentDidMount() {
     this.props.requestEmployees();
     this.props.requestTeam(this.props.match.params.id);
   }
 
-  static getDerivedStateFromProps(nextProps, prevState) {
-    if (!nextProps.team) {
-      return null;
-    } else if (!prevState.team.name && nextProps.team !== prevState.team) {
-      return { team: nextProps.team };
-    }
-    return null;
-  }
-
-  teamNameChange(event) {
-    this.setState({
-      team: {
-        ...this.state.team,
-        name: event.target.value
-      }
-    });
-  }
-
-  addMember(user) {
-    this.setState(prevState => ({
-      team: {
-        ...this.state.team,
-        users: [...prevState.team.users, user]
-      }
-    }));
-  }
-
-  addApprover(user) {
-    this.setState(prevState => ({
-      team: {
-        ...this.state.team,
-        teamApprovers: [...prevState.team.teamApprovers, user]
-      }
-    }));
-  }
-
   render() {
     if (!this.props.employees || !this.props.team) return null;
-    const employees = this.props.employees.map(employee => {
-      return (
-        <div
-          key={employee.id}
-          className="align-items-center d-flex justify-content-between my-2"
-        >
-          <span>{employee.fullName}</span>
-          <div>
-            <button
-              onClick={() => this.addMember(employee)}
-              className="btn mr-2"
-            >
-              Member
-            </button>
-            <button onClick={() => this.addApprover(employee)} className="btn">
-              Approver
-            </button>
-          </div>
-        </div>
-      );
-    });
-
-    const selectedUsers = this.state.team.users.map(employee => {
-      return <h6 key={employee.id}>{employee.fullName}</h6>;
-    });
-
-    const selectedApprovers = this.state.team.teamApprovers.map(employee => {
-      return <h6 key={employee.id}>{employee.fullName}</h6>;
-    });
-
     return (
       <div className="d-flex p-4 justify-content-center flex-column">
-        <input
-          type="text"
-          value={this.state.team.name}
-          onChange={e => this.teamNameChange(e)}
-          placeholder="Team name"
-          className="mb-3"
+        <Form
+          initialValues={this.props.team}
+          onSubmit={this.props.updateTeam}
+          mutators={{
+            ...arrayMutators
+          }}
+          render={({
+            handleSubmit,
+            values,
+            form: {
+              mutators: { push }
+            }
+          }) => (
+            <React.Fragment>
+              <Field className="mb-3" name="name" component="input" />
+              {this.props.employees.map(employee => {
+                return (
+                  <label
+                    className="d-flex justify-content-between"
+                    key={employee.id}
+                  >
+                    {employee.fullName}
+                    <div className="mb-2">
+                      <button
+                        className="btn mr-2"
+                        type="button"
+                        onClick={() => push('users', employee)}
+                      >
+                        Add member
+                      </button>
+                      <button
+                        className="btn"
+                        type="button"
+                        onClick={() => push('teamApprovers', employee)}
+                      >
+                        Add approver
+                      </button>
+                    </div>
+                  </label>
+                );
+              })}
+              <div className="d-flex justify-content-between mt-3">
+                <div className="mb-5">
+                  <h3>Team members</h3>
+                  {values.users.map(user => {
+                    return <h5 key={user.id}>{user.fullName}</h5>;
+                  })}
+                </div>
+                <div className="mb-5">
+                  <h3>Team approvers</h3>
+                  {values.teamApprovers.map(user => {
+                    return <h5 key={user.id}>{user.fullName}</h5>;
+                  })}
+                </div>
+              </div>
+              <button className="btn" onClick={handleSubmit} type="submit">
+                Update
+              </button>
+            </React.Fragment>
+          )}
         />
-        {employees}
-        <div className="d-flex justify-content-between">
-          <div className="mb-5">
-            <h3>Team members</h3>
-            {selectedUsers}
-          </div>
-          <div className="mb-5">
-            <h3>Team approvers</h3>
-            {selectedApprovers}
-          </div>
-        </div>
-        <button
-          onClick={() => this.props.updateTeam(this.state.team)}
-          className="btn"
-        >
-          Update
-        </button>
       </div>
     );
   }
