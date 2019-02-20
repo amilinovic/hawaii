c9e6fba462b6523218c164b64ecf32d644bc86e5
Aleksandar Beronja <aberonja@execom.eu>
2019-02-20 09:04:34 +0100

    refactored employeeservices

--- src/main/frontend/src/store/services/employeeService.js
+++ src/main/frontend/src/store/services/employeeService.js
@@ -1,31 +1,9 @@
-import request from 'superagent';
+import { delFactory, getFactory, postFactory, putFactory } from './request';
+const apiEndpoint = '/users';
 
-export const createEmployeeApi = async employeeObject => {
-  return await request
-    .post(`/users`)
-    .send(employeeObject)
-    .set('X-ID-TOKEN', sessionStorage.getItem('token'))
-    .then(res => res.body);
-};
-
-export const getEmployeeApi = async id => {
-  return await request
-    .get(`/users/${id}`)
-    .set('X-ID-TOKEN', sessionStorage.getItem('token'))
-    .then(res => res.body);
-};
-
-export const updateEmployeeApi = async employeeObject => {
-  return await request
-    .put('/users')
-    .send(employeeObject)
-    .set('X-ID-TOKEN', sessionStorage.getItem('token'))
-    .then(res => res.body);
-};
-
-export const removeEmployeeApi = async id => {
-  return await request
-    .del(`/users/${id}`)
-    .set('X-ID-TOKEN', sessionStorage.getItem('token'))
-    .then(res => res.body);
-};
+export const getEmployeeApi = id => getFactory(`${apiEndpoint}/${id}`);
+export const removeEmployeeApi = id => delFactory(`${apiEndpoint}/${id}`);
+export const createEmployeeApi = employeeObject =>
+  postFactory(apiEndpoint, employeeObject);
+export const updateEmployeeApi = employeeObject =>
+  putFactory(apiEndpoint, employeeObject);
--- src/main/frontend/src/store/services/request.js
+++ src/main/frontend/src/store/services/request.js
@@ -8,4 +8,30 @@ const get = (url, user = getTokenFromSessionStorage().token) => {
     .then(res => res.body);
 };
 
+const post = (url, data, user = getTokenFromSessionStorage().token) => {
+  return request
+    .post(url)
+    .send(data)
+    .set('X-ID-TOKEN', user)
+    .then(res => res.body);
+};
+
+const put = (url, data, user = getTokenFromSessionStorage().token) => {
+  return request
+    .put(url)
+    .send(data)
+    .set('X-ID-TOKEN', user)
+    .then(res => res.body);
+};
+
+const del = (url, user = getTokenFromSessionStorage().token) => {
+  return request
+    .del(url)
+    .set('X-ID-TOKEN', user)
+    .then(res => res.body);
+};
+
 export const getFactory = url => async () => await get(url);
+export const delFactory = url => async () => await del(url);
+export const postFactory = (url, data) => async () => await post(url, data);
+export const putFactory = (url, data) => async () => await put(url, data);
