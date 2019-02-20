9928e4f11562ba3b17de418747a4f0243f4cafe4
Aleksandar Beronja <aberonja@execom.eu>
2019-02-20 09:15:18 +0100

    refactored teams service

--- src/main/frontend/src/store/services/teamsService.js
+++ src/main/frontend/src/store/services/teamsService.js
@@ -1,17 +1,3 @@
-import request from 'superagent';
+import { getFactory } from './request';
 
-export const getTeamsApi = async () => {
-  try {
-    return await request
-      .get('/teams')
-      .set('X-ID-TOKEN', sessionStorage.getItem('token'))
-      .then(res => res.body)
-      .catch(err => {
-        console.log(err);
-      });
-  } catch (e) {
-    // TODO error handling
-    console.log(e);
-  }
-  return false;
-};
+export const getTeamsApi = getFactory('/teams');
