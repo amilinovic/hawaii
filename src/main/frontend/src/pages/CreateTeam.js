ba06191db589ba4d0a65f23bad3dceef8256b30e
Aleksandar Beronja <aberonja@execom.eu>
2019-02-26 15:56:42 +0100

    added createTeam form validation

--- src/main/frontend/src/pages/CreateTeam.js
+++ src/main/frontend/src/pages/CreateTeam.js
@@ -2,30 +2,42 @@ import { Formik } from 'formik';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import * as Yup from 'yup';
 import withResetOnNavigate from '../components/HOC/withResetOnNavigate';
 import SearchDropdown from '../components/search-dropdown/SearchDropdown';
 import { requestEmployees } from '../store/actions/employeesActions';
 import { createTeam } from '../store/actions/teamActions';
 import { getSearchEmployees } from '../store/selectors';
 
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required('Required')
+});
+
 class CreateTeam extends Component {
   render() {
     return (
       <div className="d-flex p-4 justify-content-center flex-column">
         <Formik
+          validationSchema={validationSchema}
           initialValues={{
             teamApprovers: [],
             users: []
           }}
           onSubmit={this.props.createTeam}
-          render={({ handleSubmit, handleChange, values }) => (
+          render={({ handleSubmit, handleChange, values, errors, touched }) => (
             <React.Fragment>
-              <input
-                className="mb-3"
-                name="name"
-                onChange={handleChange}
-                placeholder="Team name"
-              />
+              {console.log(touched)}
+              <div className="mb-3">
+                <input
+                  className="w-100"
+                  name="name"
+                  onChange={handleChange}
+                  placeholder="Team name"
+                />
+                {errors.name && touched.name ? (
+                  <div className="text-danger">{errors.name}</div>
+                ) : null}
+              </div>
               <SearchDropdown results={this.props.employees} />
               <div className="d-flex justify-content-between mt-3">
                 <div className="mb-5">
