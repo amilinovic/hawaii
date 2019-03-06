c73897a62eb73a993e4f8a14c5473b23c16e57ff
Aleksandar Beronja <35724583+aberonja@users.noreply.github.com>
2019-03-06 13:25:54 +0100

    added formik to the leave request and sickness request component

--- src/main/frontend/src/components/modal/requests/LeaveRequest.js
+++ src/main/frontend/src/components/modal/requests/LeaveRequest.js
@@ -1,54 +1,79 @@
+import { Formik } from 'formik';
 import React, { Component } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import * as Yup from 'yup';
 import Button from '../../common/Button';
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required()
+});
 
 class LeaveRequest extends Component {
   state = {
-    startDate: new Date(),
-    endDate: new Date()
+    startDate: new Date()
   };
 
-  handleChange = (date, type) => {
-    console.log(type, date);
-    // this.setState({
-    //   [type]: date
-    // });
+  handleChange = date => {
+    this.setState({
+      startDate: date
+    });
   };
 
   render() {
     return (
-      <div className="d-flex justify-content-between">
-        <div className="px-2">
-          <div className="d-flex justify-content-between mb-2">
-            <label htmlFor="leaveType">Type of Leave</label>
-            <select name="leaveType">
-              <option value="test">Annual leave</option>
-            </select>
-          </div>
-          <div className="d-flex justify-content-between mb-2">
-            <label htmlFor="startDate">Start date</label>
-            <DatePicker
-              selected={this.state.startDate}
-              onChange={this.handleChange}
-              test="test"
-            />
-          </div>
-          <div className="d-flex justify-content-between mb-2">
+      <Formik
+        validationSchema={validationSchema}
+        initialValues={{
+          name: '',
+          teamApprovers: [],
+          users: [],
+          sendEmailToTeammatesForBonusRequestEnabled: false,
+          sendEmailToTeammatesForSicknessRequestEnabled: false,
+          sendEmailToTeammatesForAnnualRequestEnabled: false
+        }}
+        onSubmit={this.props.createTeam}
+        render={({
+          handleSubmit,
+          handleChange,
+          values,
+          errors,
+          touched,
+          setFieldValue
+        }) => (
+          <div className="d-flex justify-content-between">
+            <div className="px-2">
+              <div className="d-flex justify-content-between mb-2">
+                <label htmlFor="leaveType">Type of Leave</label>
+                <select name="leaveType">
+                  <option value="test">Annual leave</option>
+                </select>
+              </div>
+              <div className="d-flex justify-content-between mb-2">
+                <label htmlFor="startDate">Start date</label>
+                <DatePicker
+                  selected={this.state.startDate}
+                  onChange={this.handleChange}
+                  test="test"
+                />
+              </div>
+              {/* <div className="d-flex justify-content-between mb-2">
             <label htmlFor="endDate">End date</label>
             <DatePicker
               selected={this.state.endDate}
               onChange={this.handleChange}
             />
+          </div> */}
+            </div>
+            <div className="px-2 d-flex flex-column">
+              <textarea className="mb-3" name="" id="" rows="10" />
+              <div className="d-flex justify-content-end">
+                <Button className="btn btn-danger" title="Apply Leave" />
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="px-2 d-flex flex-column">
-          <textarea className="mb-3" name="" id="" rows="10" />
-          <div className="d-flex justify-content-end">
-            <Button className="btn btn-danger" title="Apply Leave" />
-          </div>
-        </div>
-      </div>
+        )}
+      />
     );
   }
 }
--- src/main/frontend/src/components/modal/requests/SicknessRequest.js
+++ src/main/frontend/src/components/modal/requests/SicknessRequest.js
@@ -1,7 +1,111 @@
+import { Formik } from 'formik';
 import React, { Component } from 'react';
+import Switch from 'react-switch';
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required()
+});
 
 export default class SicknessRequest extends Component {
   render() {
-    return <div>Sickness request </div>;
+    return (
+      <div>
+        <Formik
+          validationSchema={validationSchema}
+          initialValues={{
+            name: '',
+            teamApprovers: [],
+            users: [],
+            sendEmailToTeammatesForBonusRequestEnabled: false,
+            sendEmailToTeammatesForSicknessRequestEnabled: false,
+            sendEmailToTeammatesForAnnualRequestEnabled: false
+          }}
+          onSubmit={this.props.createTeam}
+          render={({
+            handleSubmit,
+            handleChange,
+            values,
+            errors,
+            touched,
+            setFieldValue
+          }) => (
+            <React.Fragment>
+              <input
+                className={`${
+                  errors.name && touched.name ? 'border-danger' : ''
+                } w-100 border mb-3`}
+                name="name"
+                onChange={handleChange}
+                placeholder="Team name"
+              />
+              <input
+                className="w-100 border mb-3"
+                name="sicknessRequestEmails"
+                defaultValue={values.sicknessRequestEmails}
+                placeholder="Sickness request emails"
+                onChange={handleChange}
+              />
+              <input
+                className="w-100 border mb-3"
+                name="annualRequestEmails"
+                defaultValue={values.annualRequestEmails}
+                placeholder="Annual request emails"
+                onChange={handleChange}
+              />
+              <input
+                className="w-100 border mb-3"
+                name="bonusRequestEmails"
+                defaultValue={values.bonusRequestEmails}
+                placeholder="Bonus request emails"
+                onChange={handleChange}
+              />
+              <div className="d-flex justify-content-between mb-3">
+                <h5>Send email to teammates for annual request</h5>
+                <Switch
+                  onChange={() =>
+                    setFieldValue(
+                      'sendEmailToTeammatesForAnnualRequestEnabled',
+                      !values.sendEmailToTeammatesForAnnualRequestEnabled
+                    )
+                  }
+                  checked={values.sendEmailToTeammatesForAnnualRequestEnabled}
+                  name="sendEmailToTeammatesForAnnualRequestEnabled"
+                />
+              </div>
+              <div className="d-flex justify-content-between mb-3">
+                <h5>Send email to teammates for bonus request</h5>
+                <Switch
+                  onChange={() =>
+                    setFieldValue(
+                      'sendEmailToTeammatesForBonusRequestEnabled',
+                      !values.sendEmailToTeammatesForBonusRequestEnabled
+                    )
+                  }
+                  checked={values.sendEmailToTeammatesForBonusRequestEnabled}
+                  name="sendEmailToTeammatesForBonusRequestEnabled"
+                />
+              </div>
+              <div className="d-flex justify-content-between mb-3">
+                <h5>Send email to teammates for bonus request</h5>
+                <Switch
+                  onChange={() =>
+                    setFieldValue(
+                      'sendEmailToTeammatesForSicknessRequestEnabled',
+                      !values.sendEmailToTeammatesForSicknessRequestEnabled
+                    )
+                  }
+                  checked={values.sendEmailToTeammatesForSicknessRequestEnabled}
+                  name="sendEmailToTeammatesForSicknessRequestEnabled"
+                />
+              </div>
+              <button className="btn" onClick={handleSubmit} type="submit">
+                Create team
+              </button>
+            </React.Fragment>
+          )}
+        />
+      </div>
+    );
   }
 }
