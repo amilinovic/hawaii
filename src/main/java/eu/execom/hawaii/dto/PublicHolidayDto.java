2c02044438d5483275bf21d47f77755fe19d03dc
Danilo petrovic <petrovicdanilo1996@gmail.com>
2019-03-13 10:29:41 +0100

    Get public holidays reguestParams set default value and add id to public holiday dto

--- src/main/java/eu/execom/hawaii/dto/PublicHolidayDto.java
+++ src/main/java/eu/execom/hawaii/dto/PublicHolidayDto.java
@@ -8,11 +8,13 @@ import java.time.LocalDate;
 @Data
 public class PublicHolidayDto {
 
+  private Long id;
   private boolean deleted;
   private String name;
   private LocalDate date;
 
   public PublicHolidayDto(PublicHoliday publicHoliday) {
+    this.id = publicHoliday.getId();
     this.deleted = publicHoliday.isDeleted();
     this.name = publicHoliday.getName();
     this.date = publicHoliday.getDate();
