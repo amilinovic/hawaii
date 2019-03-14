5d8d890aa69006a9d936a5101f5bcdc932df6724
bexaca <33089360+bexaca@users.noreply.github.com>
2019-03-14 21:28:02 +0100

    added a bit of styling to the infoCard

--- src/main/frontend/src/components/info-card/InfoCard.js
+++ src/main/frontend/src/components/info-card/InfoCard.js
@@ -12,7 +12,7 @@ export default class InfoCard extends Component {
 
     const cardInfo = this.props.allowance.map(item => {
       return (
-        <div key={item.id} className="d-flex justify-content-between">
+        <div key={item.id} className="d-flex justify-content-between mb-2">
           <span>{item.type}</span>
           <span>{item.hours} hours</span>
         </div>
@@ -20,8 +20,8 @@ export default class InfoCard extends Component {
     });
 
     return (
-      <Card className="p-3 border">
-        <h1 className="border-bottom mb-3">{this.props.title}</h1>
+      <Card className="p-3 border rounded">
+        <h1 className="border-bottom mb-3 pb-1">{this.props.title}</h1>
         {cardInfo}
       </Card>
     );
